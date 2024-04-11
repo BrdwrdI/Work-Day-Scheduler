0bde7735982819e45938560f6a40f601c2e7fa94
WRD <dandobilly@gmail.com>
2024-04-10 22:36:44 -0400

    made some changes to codebase

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,42 +3,5 @@ const saveBtn = $('.saveBtn');
 const currentHour = dayjs().format('HH');
 
 $(function () {
-  
-  function dateTimeDisplay() {
-    const now = dayjs().format('MMMM DD, YYYY hh:mm:ss');
-    dateEl.text(now);
-  }
-  setInterval(dateTimeDisplay, 500);
 
-  saveBtn.on('click', function() {
-    const timeBlock = $(this).parent().attr('id');
-    const blockText = $(this).siblings('description').val();
-    localStorage.setItem(timeBlock, blockText);
-  });
-
-  function blockColor() {
-    $('.time-block').each(function () {
-      const inputString = $(this).attr('id');
-      const number = parseInt(inputString.split('-')[1]);
-      if (number < currentHour) {
-        $(this).addClass('past');
-      }
-      if (number === currentHour) {
-        $(this).addClass('present');
-      }
-      if (number > currentHour) {
-        $(this).addClass('future');
-      };
-    });
-  };
-  blockColor();
-
-  function loadTasks() {
-    $('.description').each(function () {
-      const timeBlock = $(this).parent().attr('id');
-      const task = localStorage.getItem(timeBlock);
-      $(this).val(task);
-    });
-  };
-  loadTasks();
 });
